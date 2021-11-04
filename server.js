a2a84009db0d8671734bd67b1b39fd45c454cb07
Karina Williams <coleman_karina@yahoo.com>
2021-11-04 12:25:37 -0700

    deploying

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 3005;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
