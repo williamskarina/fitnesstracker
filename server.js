f61518f25ef13dcadaf8d11b7ad0cc1071fb0f29
Karina Williams <coleman_karina@yahoo.com>
2021-11-09 12:48:51 -0800

    deploying

--- server.js
+++ server.js
@@ -13,10 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 
 require("./routes/htmlroutes")(app);
